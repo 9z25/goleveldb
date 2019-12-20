90f45c5e95e2e1718c39022c6b677c67ab33f126
9z25 <rsenecaljr@gmail.com>
2019-12-19 17:13:45 -0800

    Update batch.go

--- leveldb/batch.go
+++ leveldb/batch.go
@@ -11,9 +11,9 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/btcsuite/goleveldb/leveldb/errors"
-	"github.com/btcsuite/goleveldb/leveldb/memdb"
-	"github.com/btcsuite/goleveldb/leveldb/storage"
+	"github.com/9z25/goleveldb/leveldb/errors"
+	"github.com/9z25/goleveldb/leveldb/memdb"
+	"github.com/9z25/goleveldb/leveldb/storage"
 )
 
 // ErrBatchCorrupted records reason of batch corruption. This error will be
