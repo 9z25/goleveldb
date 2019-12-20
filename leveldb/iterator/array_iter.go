f5c83584e1bed98da6847c3a05cdc04872f68fe2
9z25 <rsenecaljr@gmail.com>
2019-12-19 17:03:24 -0800

    Update array_iter.go

--- leveldb/iterator/array_iter.go
+++ leveldb/iterator/array_iter.go
@@ -7,7 +7,7 @@
 package iterator
 
 import (
-	"github.com/btcsuite/goleveldb/leveldb/util"
+	"github.com/9z25/goleveldb/leveldb/util"
 )
 
 // BasicArray is the interface that wraps basic Len and Search method.
