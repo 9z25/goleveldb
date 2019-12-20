39528651fe492a2fd15c3eed1b2c25d3c7add663
9z25 <rsenecaljr@gmail.com>
2019-12-19 17:14:02 -0800

    Update batch_test.go

--- leveldb/batch_test.go
+++ leveldb/batch_test.go
@@ -12,7 +12,7 @@ import (
 	"testing"
 	"testing/quick"
 
-	"github.com/btcsuite/goleveldb/leveldb/testutil"
+	"github.com/9z25/goleveldb/leveldb/testutil"
 )
 
 func TestBatchHeader(t *testing.T) {
