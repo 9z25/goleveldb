3bd695f2c4375167dae3e4a19034b87d222a0643
9z25 <rsenecaljr@gmail.com>
2019-12-19 17:20:57 -0800

    Update key_test.go

--- leveldb/key_test.go
+++ leveldb/key_test.go
@@ -10,7 +10,7 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/btcsuite/goleveldb/leveldb/comparer"
+	"github.com/9z25/goleveldb/leveldb/comparer"
 )
 
 var defaultIComparer = &iComparer{comparer.DefaultComparer}
