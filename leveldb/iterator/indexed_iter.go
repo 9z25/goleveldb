ce3681cd3d8ffaf2efb064357540428b88187c05
9z25 <rsenecaljr@gmail.com>
2019-12-19 17:03:39 -0800

    Update indexed_iter.go

--- leveldb/iterator/indexed_iter.go
+++ leveldb/iterator/indexed_iter.go
@@ -7,8 +7,8 @@
 package iterator
 
 import (
-	"github.com/btcsuite/goleveldb/leveldb/errors"
-	"github.com/btcsuite/goleveldb/leveldb/util"
+	"github.com/9z25/goleveldb/leveldb/errors"
+	"github.com/9z25/goleveldb/leveldb/util"
 )
 
 // IteratorIndexer is the interface that wraps CommonIterator and basic Get
