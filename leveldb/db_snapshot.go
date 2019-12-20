112430eaf287d49d90aaaa22b726b7b9d6781427
9z25 <rsenecaljr@gmail.com>
2019-12-19 17:16:57 -0800

    Update db_snapshot.go

--- leveldb/db_snapshot.go
+++ leveldb/db_snapshot.go
@@ -13,9 +13,9 @@ import (
 	"sync"
 	"sync/atomic"
 
-	"github.com/btcsuite/goleveldb/leveldb/iterator"
-	"github.com/btcsuite/goleveldb/leveldb/opt"
-	"github.com/btcsuite/goleveldb/leveldb/util"
+	"github.com/9z25/goleveldb/leveldb/iterator"
+	"github.com/9z25/goleveldb/leveldb/opt"
+	"github.com/9z25/goleveldb/leveldb/util"
 )
 
 type snapshotElement struct {
