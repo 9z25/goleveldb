236fea48f1cbd0bc2e59b2e7fe506b0346ca5778
9z25 <rsenecaljr@gmail.com>
2019-12-19 17:22:52 -0800

    Update session_record.go

--- leveldb/session_record.go
+++ leveldb/session_record.go
@@ -12,8 +12,8 @@ import (
 	"io"
 	"strings"
 
-	"github.com/btcsuite/goleveldb/leveldb/errors"
-	"github.com/btcsuite/goleveldb/leveldb/storage"
+	"github.com/9z25/goleveldb/leveldb/errors"
+	"github.com/9z25/goleveldb/leveldb/storage"
 )
 
 type byteReader interface {
